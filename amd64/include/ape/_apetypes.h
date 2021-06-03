2bb9c4904b784f9a85554b3ef6a5fb771acc79a3
lufia <lufia@lufia.org>
2021-06-03 09:37:47 +0900

    sys/include/ape: add BYTE_ORDER

--- amd64/include/ape/_apetypes.h
+++ amd64/include/ape/_apetypes.h
@@ -1,11 +1,3 @@
 #ifndef _BITS64
 #define _BITS64
 #endif
-
-#if !defined(__BYTE_ORDER) && defined(__LITTLE_ENDIAN)
-#define	__BYTE_ORDER	__LITTLE_ENDIAN
-#endif
-
-#if !defined(BYTE_ORDER) && defined(LITTLE_ENDIAN)
-#define	BYTE_ORDER	LITTLE_ENDIAN
-#endif
